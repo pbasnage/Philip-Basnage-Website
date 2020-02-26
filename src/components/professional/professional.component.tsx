ff16a00db01a34e528832143f3bfc82751e5b598
pbasnagetr <philip@treering.com>
2020-02-26 14:18:17 -0800

    adding education

--- src/components/professional/professional.component.tsx
+++ src/components/professional/professional.component.tsx
@@ -3,24 +3,45 @@ import ReactDOM from 'react-dom';
 import { ProfessionalProps } from '../../interfaces/professional.interface';
 import '../../main.scss';
 import './professional.component.scss';
+import LehighCrest from '../../images/lehighcrest.png';
 
 export class Professional extends React.Component {
+
     constructor(props: ProfessionalProps) {
         super(props);
+
     }
     render(): JSX.Element {
         return (
-            <div>
+            <div className="professional-wrapper"> 
                 <div className="main-header">Philip Basnage: Professional</div>
-                <div className="summary">
-                    <div className="summary-title">Professional Summary</div>
-                    <div className="summary-body">Consumer-focused software engineer with a 
-                    demonstrated history of working in the internet industry and over 3 
-                    years of professional experience in the field. Skilled in modern web 
-                    development, user experience, user interfaces, and responsive design. 
-                    Strong, collaborative engineering professional with a Bachelor's 
-                    degree in Computer Science from Lehigh University.
-                </div>
+                <div className="panels-container">
+                    <div className="panel">
+                        <div className="panel-title">
+                            Professional Summary
+                        </div>
+                        <div className="panel-body">
+                            Consumer-focused software engineer with a 
+                            demonstrated history of working in the internet industry and over 3 
+                            years of professional experience in the field. Skilled in modern web 
+                            development, user experience, user interfaces, and responsive design. 
+                            Strong, collaborative engineering professional with a Bachelor's 
+                            degree in Computer Science from Lehigh University.
+                        </div>
+                    </div>
+                    <div className="panel-edu">
+                        <div className="flex-column">
+                            <div className="panel-title">
+                                Education
+                            </div>
+                            <div className="panel-body">
+                                Graduated from Lehigh University in May 2017 with a Bachelor's
+                                of Science degree in Computer Science from PC Rossin College of
+                                Engineering and Applied Science.
+                            </div>
+                        </div>
+                        <img src={LehighCrest} alt="" className="lehigh-crest"/>
+                    </div>
                 </div>
             </div>
         );
